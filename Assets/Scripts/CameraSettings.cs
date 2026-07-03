420e7ca3529e5f7e00061ee6e7edaaebc62ffb65
Jason <128804387+UICJason0505@users.noreply.github.com>
2026-07-04 04:39:02 +0800

    CameraFunctionalModel(v1)

---
+++ Assets/Scripts/CameraSettings.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSettings : MonoBehaviour
+{
+    [System.Serializable]
+    public struct CameraData
+    {
+        public float xminus;     
+        public float xplus;      
+        public float moveSpeed; 
+    }
+
+    public CameraData data;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            pos.x -= data.moveSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            pos.x += data.moveSpeed * Time.deltaTime;
+        }
+
+        pos.x = Mathf.Clamp(pos.x, data.xminus, data.xplus);
+
+        transform.position = pos;
+    }
+}
